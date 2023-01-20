3110ab8567fa9a0edb3bed45c24f0cd17a31cf77
Philip <17368112+vHeemstra@users.noreply.github.com>
2023-01-20 15:45:20 +0100

    Update index.test.ts

--- packages/core/src/index.test.ts
+++ packages/core/src/index.test.ts
@@ -1458,7 +1458,7 @@ describe.skipIf(skipBuilds)('viteImagemin', () => {
       // - check if certain files have been skipped
     },
     {
-      timeout: 10000,
+      timeout: 30000,
     },
   )
 
